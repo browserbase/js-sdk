65f9712285d1b5e00a7be01ec1c50883788455aa
Garrett Graves <garrett@browserbase.com>
2024-07-09 15:32:49 -0700

    split out browser bundling

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-npm run build
+# NOTE: This script is used to bundle the recorder.js file for the browser.
+# It is highly experimental and may not work as expected. Use at your own risk.
 
-browserify dist/recorder/recorder.js -o dist/browser/bundle.js
+npm run build:browser
+
+browserify dist/browser/tsc/recorder.js -o dist/browser/bundle.js
 
 # Use sed to insert the new string after the line containing the search string
 sed -i '' "/exports.BrowserbaseCodeGenerator = BrowserbaseCodeGenerator;/a\\
